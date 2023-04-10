f97377fa74c54b883a96bc5f37987ca3a3bfcda4
Paulo Gonzalez <paulotony100@gmail.com>
2023-04-10 15:19:09 +0200

    Se Agrega el Proyecto de OpenBootCamp

---
+++ src/Coche.java
@@ -0,0 +1,13 @@
+public class Coche {
+    int numeroPuertas;
+    public Coche(int puertas) {
+        numeroPuertas = puertas;
+    }
+
+    public int incrementarPuertas(){
+        numeroPuertas ++;
+
+    return numeroPuertas;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    /* Ejercicios Tema #3._
+
+    Enunciado del ejercicio:
+    Primera parte:
+    -Crear una función con tres parámetros que sean números que se suman entre sí.
+    -Llamar a la función en el main y darle valores.
+    Segunda parte:
+    -Crear una clase coche.
+    -Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+    -Una función que incremente el número de puertas que tiene el coche.
+    -Crear un objeto miCoche en el main y añadirle una puerta.
+    -Mostrar el número de puertas que tiene el objeto. */
+
+
+    public static void main(String[] args)
+    {
+        Coche miCoche = new Coche(1);
+
+        miCoche.incrementarPuertas();
+        System.out.println(miCoche.numeroPuertas);
+
+        int result = sumaFuncion(5, 10, 15);
+        System.out.println(result);
+    }
+
+    public static int sumaFuncion(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+}
